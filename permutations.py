87a492bcb787ed549df5b97fdf95237f89481079
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-08-25 14:15:23 +0300

    Permutations added

--- permutations.py
+++ permutations.py
@@ -1,10 +1,35 @@
 __author__ = 'cenk'
 
+class Solution(object):
+    solutions = []
 
-def permutations(nums):
-    pass
+    def generateCandidates(self,nums,current):
+        s = set(nums)
+        currentSet = set(current)
+        candidates = s - currentSet
+        return list(candidates)
 
+    def permute(self,nums):
+        self.solutions  = []
 
+        self.permutationsHelper(nums,[])
+        return self.solutions
 
-s = [1,2,3]
-print permutations(s)
+    def permutationsHelper(self,nums,current):
+        # is solution
+        if len(current) == len(nums):
+            # tmp = "".join([str(i) for i in current])
+            tmp = current[:]
+            self.solutions.append(tmp)
+            return
+
+        candidates = self.generateCandidates(nums,current)
+
+        for i in candidates:
+            current.append(i)
+            self.permutationsHelper(nums,current)
+            current.pop()
+
+s = Solution()
+a=  [0,1]
+print s.permute(a)
